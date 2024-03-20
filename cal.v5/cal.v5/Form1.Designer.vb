6b6fd7fd0e870581228220eb288ff92d65824651
jb_Srfca <163223246+Srfca@users.noreply.github.com>
2024-03-19 18:08:20 -0700

    Add files via upload

---
+++ cal.v5/cal.v5/Form1.Designer.vb
@@ -0,0 +1,326 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DelBtn = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.CEBtn = New System.Windows.Forms.Button()
+        Me.ClrBtn = New System.Windows.Forms.Button()
+        Me.Btn = New System.Windows.Forms.Button()
+        Me.Btnsum = New System.Windows.Forms.Button()
+        Me.Btn9 = New System.Windows.Forms.Button()
+        Me.Btn8 = New System.Windows.Forms.Button()
+        Me.Btn7 = New System.Windows.Forms.Button()
+        Me.Btndiff = New System.Windows.Forms.Button()
+        Me.Btn6 = New System.Windows.Forms.Button()
+        Me.Btn5 = New System.Windows.Forms.Button()
+        Me.Btn4 = New System.Windows.Forms.Button()
+        Me.Btnprod = New System.Windows.Forms.Button()
+        Me.Btn3 = New System.Windows.Forms.Button()
+        Me.Btn2 = New System.Windows.Forms.Button()
+        Me.Btn1 = New System.Windows.Forms.Button()
+        Me.Btndiv = New System.Windows.Forms.Button()
+        Me.BtnEqual = New System.Windows.Forms.Button()
+        Me.BtnDec = New System.Windows.Forms.Button()
+        Me.Btn0 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.White
+        Me.Label1.Font = New System.Drawing.Font("Arial Rounded MT Bold", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(368, 40)
+        Me.Label1.TabIndex = 0
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'DelBtn
+        '
+        Me.DelBtn.Font = New System.Drawing.Font("Bahnschrift", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DelBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.DelBtn.Location = New System.Drawing.Point(12, 138)
+        Me.DelBtn.Name = "DelBtn"
+        Me.DelBtn.Size = New System.Drawing.Size(85, 80)
+        Me.DelBtn.TabIndex = 1
+        Me.DelBtn.Text = "⌫"
+        Me.DelBtn.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.White
+        Me.Label2.Font = New System.Drawing.Font("Arial Rounded MT Bold", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 70)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(368, 40)
+        Me.Label2.TabIndex = 2
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'CEBtn
+        '
+        Me.CEBtn.Font = New System.Drawing.Font("Bahnschrift", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CEBtn.Location = New System.Drawing.Point(103, 138)
+        Me.CEBtn.Name = "CEBtn"
+        Me.CEBtn.Size = New System.Drawing.Size(85, 80)
+        Me.CEBtn.TabIndex = 3
+        Me.CEBtn.Text = "CE"
+        Me.CEBtn.UseVisualStyleBackColor = True
+        '
+        'ClrBtn
+        '
+        Me.ClrBtn.Font = New System.Drawing.Font("Bahnschrift", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ClrBtn.Location = New System.Drawing.Point(203, 138)
+        Me.ClrBtn.Name = "ClrBtn"
+        Me.ClrBtn.Size = New System.Drawing.Size(85, 80)
+        Me.ClrBtn.TabIndex = 4
+        Me.ClrBtn.Text = "C"
+        Me.ClrBtn.UseVisualStyleBackColor = True
+        '
+        'Btn
+        '
+        Me.Btn.Font = New System.Drawing.Font("Bahnschrift", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn.Location = New System.Drawing.Point(295, 138)
+        Me.Btn.Name = "Btn"
+        Me.Btn.Size = New System.Drawing.Size(85, 80)
+        Me.Btn.TabIndex = 5
+        Me.Btn.Text = "±"
+        Me.Btn.UseVisualStyleBackColor = True
+        '
+        'Btnsum
+        '
+        Me.Btnsum.Font = New System.Drawing.Font("Bahnschrift", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btnsum.Location = New System.Drawing.Point(294, 224)
+        Me.Btnsum.Name = "Btnsum"
+        Me.Btnsum.Size = New System.Drawing.Size(85, 80)
+        Me.Btnsum.TabIndex = 9
+        Me.Btnsum.Text = "+"
+        Me.Btnsum.UseVisualStyleBackColor = True
+        '
+        'Btn9
+        '
+        Me.Btn9.Font = New System.Drawing.Font("Bahnschrift", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn9.Location = New System.Drawing.Point(203, 224)
+        Me.Btn9.Name = "Btn9"
+        Me.Btn9.Size = New System.Drawing.Size(85, 80)
+        Me.Btn9.TabIndex = 8
+        Me.Btn9.Text = "9"
+        Me.Btn9.UseVisualStyleBackColor = True
+        '
+        'Btn8
+        '
+        Me.Btn8.Font = New System.Drawing.Font("Bahnschrift", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn8.Location = New System.Drawing.Point(102, 224)
+        Me.Btn8.Name = "Btn8"
+        Me.Btn8.Size = New System.Drawing.Size(85, 80)
+        Me.Btn8.TabIndex = 7
+        Me.Btn8.Text = "8"
+        Me.Btn8.UseVisualStyleBackColor = True
+        '
+        'Btn7
+        '
+        Me.Btn7.Font = New System.Drawing.Font("Bahnschrift", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn7.Location = New System.Drawing.Point(11, 224)
+        Me.Btn7.Name = "Btn7"
+        Me.Btn7.Size = New System.Drawing.Size(85, 80)
+        Me.Btn7.TabIndex = 6
+        Me.Btn7.Text = "7"
+        Me.Btn7.UseVisualStyleBackColor = True
+        '
+        'Btndiff
+        '
+        Me.Btndiff.Font = New System.Drawing.Font("Bahnschrift", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btndiff.Location = New System.Drawing.Point(294, 310)
+        Me.Btndiff.Name = "Btndiff"
+        Me.Btndiff.Size = New System.Drawing.Size(85, 80)
+        Me.Btndiff.TabIndex = 13
+        Me.Btndiff.Text = "-"
+        Me.Btndiff.UseVisualStyleBackColor = True
+        '
+        'Btn6
+        '
+        Me.Btn6.Font = New System.Drawing.Font("Bahnschrift", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn6.Location = New System.Drawing.Point(203, 310)
+        Me.Btn6.Name = "Btn6"
+        Me.Btn6.Size = New System.Drawing.Size(85, 80)
+        Me.Btn6.TabIndex = 12
+        Me.Btn6.Text = "6"
+        Me.Btn6.UseVisualStyleBackColor = True
+        '
+        'Btn5
+        '
+        Me.Btn5.Font = New System.Drawing.Font("Bahnschrift", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn5.Location = New System.Drawing.Point(102, 310)
+        Me.Btn5.Name = "Btn5"
+        Me.Btn5.Size = New System.Drawing.Size(85, 80)
+        Me.Btn5.TabIndex = 11
+        Me.Btn5.Text = "5"
+        Me.Btn5.UseVisualStyleBackColor = True
+        '
+        'Btn4
+        '
+        Me.Btn4.Font = New System.Drawing.Font("Bahnschrift", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn4.Location = New System.Drawing.Point(11, 310)
+        Me.Btn4.Name = "Btn4"
+        Me.Btn4.Size = New System.Drawing.Size(85, 80)
+        Me.Btn4.TabIndex = 10
+        Me.Btn4.Text = "4"
+        Me.Btn4.UseVisualStyleBackColor = True
+        '
+        'Btnprod
+        '
+        Me.Btnprod.Font = New System.Drawing.Font("Bahnschrift", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btnprod.Location = New System.Drawing.Point(294, 396)
+        Me.Btnprod.Name = "Btnprod"
+        Me.Btnprod.Size = New System.Drawing.Size(85, 80)
+        Me.Btnprod.TabIndex = 17
+        Me.Btnprod.Text = "*"
+        Me.Btnprod.UseVisualStyleBackColor = True
+        '
+        'Btn3
+        '
+        Me.Btn3.Font = New System.Drawing.Font("Bahnschrift", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn3.Location = New System.Drawing.Point(203, 396)
+        Me.Btn3.Name = "Btn3"
+        Me.Btn3.Size = New System.Drawing.Size(85, 80)
+        Me.Btn3.TabIndex = 16
+        Me.Btn3.Text = "3"
+        Me.Btn3.UseVisualStyleBackColor = True
+        '
+        'Btn2
+        '
+        Me.Btn2.Font = New System.Drawing.Font("Bahnschrift", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn2.Location = New System.Drawing.Point(102, 396)
+        Me.Btn2.Name = "Btn2"
+        Me.Btn2.Size = New System.Drawing.Size(85, 80)
+        Me.Btn2.TabIndex = 15
+        Me.Btn2.Text = "2"
+        Me.Btn2.UseVisualStyleBackColor = True
+        '
+        'Btn1
+        '
+        Me.Btn1.Font = New System.Drawing.Font("Bahnschrift", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn1.Location = New System.Drawing.Point(11, 396)
+        Me.Btn1.Name = "Btn1"
+        Me.Btn1.Size = New System.Drawing.Size(85, 80)
+        Me.Btn1.TabIndex = 14
+        Me.Btn1.Text = "1"
+        Me.Btn1.UseVisualStyleBackColor = True
+        '
+        'Btndiv
+        '
+        Me.Btndiv.Font = New System.Drawing.Font("Bahnschrift", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btndiv.Location = New System.Drawing.Point(294, 482)
+        Me.Btndiv.Name = "Btndiv"
+        Me.Btndiv.Size = New System.Drawing.Size(85, 80)
+        Me.Btndiv.TabIndex = 21
+        Me.Btndiv.Text = "/"
+        Me.Btndiv.UseVisualStyleBackColor = True
+        '
+        'BtnEqual
+        '
+        Me.BtnEqual.Font = New System.Drawing.Font("Bahnschrift", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnEqual.Location = New System.Drawing.Point(203, 482)
+        Me.BtnEqual.Name = "BtnEqual"
+        Me.BtnEqual.Size = New System.Drawing.Size(85, 80)
+        Me.BtnEqual.TabIndex = 20
+        Me.BtnEqual.Text = "="
+        Me.BtnEqual.UseVisualStyleBackColor = True
+        '
+        'BtnDec
+        '
+        Me.BtnDec.Font = New System.Drawing.Font("Bahnschrift", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnDec.Location = New System.Drawing.Point(102, 482)
+        Me.BtnDec.Name = "BtnDec"
+        Me.BtnDec.Size = New System.Drawing.Size(85, 80)
+        Me.BtnDec.TabIndex = 19
+        Me.BtnDec.Text = "."
+        Me.BtnDec.UseVisualStyleBackColor = True
+        '
+        'Btn0
+        '
+        Me.Btn0.Font = New System.Drawing.Font("Bahnschrift", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn0.Location = New System.Drawing.Point(11, 482)
+        Me.Btn0.Name = "Btn0"
+        Me.Btn0.Size = New System.Drawing.Size(85, 80)
+        Me.Btn0.TabIndex = 18
+        Me.Btn0.Text = "0"
+        Me.Btn0.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(392, 600)
+        Me.Controls.Add(Me.Btndiv)
+        Me.Controls.Add(Me.BtnEqual)
+        Me.Controls.Add(Me.BtnDec)
+        Me.Controls.Add(Me.Btn0)
+        Me.Controls.Add(Me.Btnprod)
+        Me.Controls.Add(Me.Btn3)
+        Me.Controls.Add(Me.Btn2)
+        Me.Controls.Add(Me.Btn1)
+        Me.Controls.Add(Me.Btndiff)
+        Me.Controls.Add(Me.Btn6)
+        Me.Controls.Add(Me.Btn5)
+        Me.Controls.Add(Me.Btn4)
+        Me.Controls.Add(Me.Btnsum)
+        Me.Controls.Add(Me.Btn9)
+        Me.Controls.Add(Me.Btn8)
+        Me.Controls.Add(Me.Btn7)
+        Me.Controls.Add(Me.Btn)
+        Me.Controls.Add(Me.ClrBtn)
+        Me.Controls.Add(Me.CEBtn)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.DelBtn)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DelBtn As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents CEBtn As Button
+    Friend WithEvents ClrBtn As Button
+    Friend WithEvents Btn As Button
+    Friend WithEvents Btnsum As Button
+    Friend WithEvents Btn9 As Button
+    Friend WithEvents Btn8 As Button
+    Friend WithEvents Btn7 As Button
+    Friend WithEvents Btndiff As Button
+    Friend WithEvents Btn6 As Button
+    Friend WithEvents Btn5 As Button
+    Friend WithEvents Btn4 As Button
+    Friend WithEvents Btnprod As Button
+    Friend WithEvents Btn3 As Button
+    Friend WithEvents Btn2 As Button
+    Friend WithEvents Btn1 As Button
+    Friend WithEvents Btndiv As Button
+    Friend WithEvents BtnEqual As Button
+    Friend WithEvents BtnDec As Button
+    Friend WithEvents Btn0 As Button
+End Class
---
+++ cal.v5/cal.v5/Form1.vb
@@ -0,0 +1,100 @@
+﻿Public Class Form1
+
+    Dim num1, num2 As Double
+    Dim op As String
+
+    Private Sub NumBtn(sender As Object, e As EventArgs) Handles BtnDec.Click, Btn9.Click, Btn8.Click, Btn7.Click, Btn6.Click, Btn5.Click, Btn4.Click, Btn3.Click, Btn2.Click, Btn1.Click, Btn0.Click
+
+        Dim b As Button = sender
+
+        If (b.Text = ".") Then
+
+            If (Not Label2.Text.Contains(".")) Then
+                Label2.Text = Label2.Text + b.Text
+            End If
+        Else
+            Label2.Text = Label2.Text + b.Text
+        End If
+
+    End Sub
+
+    Private Sub OpBtn(sender As Object, e As EventArgs) Handles Btnsum.Click, Btnprod.Click, Btndiv.Click, Btndiff.Click
+
+        Dim b As Button = sender
+
+        If (Label2.Text <> 0) Then
+            Label1.Text = Label2.Text
+            Label2.Text = ""
+            op = b.Text
+            Label2.Text = Label2.Text + "" + op
+        End If
+
+    End Sub
+
+    Private Sub EqualBtn(sender As Object, e As EventArgs) Handles BtnEqual.Click
+
+        Try
+
+            If Label1.Text = ("Invalid!") Then Label1.Text = vbEmpty
+
+            If (Label2.Text <> "" And op <> "") Then
+                num1 = Val(Label1.Text) : num2 = Val(Label2.Text)
+
+                Select Case op
+                    Case "+"
+                        Label2.Text = (num1 + num2)
+                    Case "-"
+                        Label2.Text = (num1 - num2)
+                    Case "*"
+                        Label2.Text = (num1 * num2)
+                    Case "/"
+                        If num2 = 0 Then
+                            Label2.Text = ("Invalid!")
+                        Else
+                            Label2.Text = (num1 / num2)
+                        End If
+
+                End Select
+                Label1.Text = ""
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub DelBtn_Click(sender As Object, e As EventArgs) Handles DelBtn.Click
+
+        If (Label2.Text.Length > 0) Then
+            Label2.Text = Label2.Text.Remove(Label2.Text.Length - 1, 1)
+        End If
+
+    End Sub
+
+    Private Sub CEBtn_Click(sender As Object, e As EventArgs) Handles CEBtn.Click
+        Dim f, s As String
+
+        Label2.Text = ""
+        f = Convert.ToString(num1)
+        s = Convert.ToString(num2)
+
+        f = ""
+        s = ""
+
+    End Sub
+
+    Private Sub ClrBtn_Click(sender As Object, e As EventArgs) Handles ClrBtn.Click
+        Label2.Text = ""
+        Label1.Text = ""
+    End Sub
+
+    Private Sub Btn_Click(sender As Object, e As EventArgs) Handles Btn.Click
+
+        Dim pm As Double
+
+        pm = Convert.ToDouble(Label2.Text)
+        Label2.Text = Convert.ToString(-1 * pm)
+    End Sub
+
+End Class
